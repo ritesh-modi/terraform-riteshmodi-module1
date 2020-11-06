4a22154ed0f30bc58ca8662d16e0b22b6a989ace
ritesh-modi <ritesh-modi@outlook.com>
2020-11-06 14:26:54 +0530

    done

---
+++ rg.tf
@@ -0,0 +1,37 @@
+
+variable rgname1 {
+    type = "string"
+}
+
+variable rgname2 {
+    type = "string"
+}
+
+variable location1 {
+    type = "string"
+}
+
+variable location2 {
+    type = "string"
+}
+
+resource azurerm_resource_group myrg {
+    name= var.rgname1
+    location  = var.location1
+
+
+}
+
+resource azurerm_resource_group myrg1 {
+    name= var.rgname2
+    location  = var.location2
+
+}
+
+output myoutput1 {
+    value = azurerm_resource_group.myrg.id
+}
+
+output myoutput2 {
+    value = azurerm_resource_group.myrg.id
+}
